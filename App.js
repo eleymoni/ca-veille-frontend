9912587bad26df468fa7abb1f3ea24103801004a
pierreC <pierrecgdev@gmail.com>
2025-05-20 16:51:34 +0200

    header

--- App.js
+++ App.js
@@ -2,41 +2,41 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { useFonts } from "expo-font";
 import {
-  Comfortaa_600SemiBold,
-  Comfortaa_700Bold,
+    Comfortaa_600SemiBold,
+    Comfortaa_700Bold,
 } from "@expo-google-fonts/comfortaa";
 import {
-  OpenSans_400Regular,
-  OpenSans_600SemiBold,
+    OpenSans_400Regular,
+    OpenSans_600SemiBold,
 } from "@expo-google-fonts/open-sans";
 
 import theme from "../core/theme";
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    Comfortaa_600SemiBold,
-    Comfortaa_700Bold,
-    OpenSans_400Regular,
-    OpenSans_600SemiBold,
-  });
+    const [fontsLoaded] = useFonts({
+        Comfortaa_600SemiBold,
+        Comfortaa_700Bold,
+        OpenSans_400Regular,
+        OpenSans_600SemiBold,
+    });
 
-  if (!fontsLoaded) {
-    return null; // ou un <LoadingScreen />
-  }
+    if (!fontsLoaded) {
+        return null; // ou un <LoadingScreen />
+    }
 
-  return (
-    <View style={{ fontFamily: theme.fonts.comfortaaBold }}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={{ fontFamily: theme.fonts.comfortaaBold }}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
---
+++ components/Header.js
@@ -0,0 +1,159 @@
+import React, { useState } from "react";
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    StyleSheet,
+    Platform,
+    TextInput,
+    Image,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import theme from "../core/theme";
+
+const Header = ({ title, colorText = theme.colors.text_dark, searchRange }) => {
+    // search bar state
+    const [showSearch, setShowSearch] = useState(false);
+    const [searchText, setSearchText] = useState("");
+    // if the title length is too long, maxLength is the max length ofthe title
+    const maxLength = 55;
+    title =
+        title?.length > maxLength
+            ? title.substring(0, maxLength) + "..."
+            : title;
+
+    const toggleSearch = () => {
+        // switch to search on header on click of search icon
+        setShowSearch((prev) => !prev);
+        setSearchText("");
+    };
+
+    const handleSearch = () => {
+        // search action, use searchRange for defining the range of search
+        console.log(searchText);
+        setSearchText("");
+    };
+
+    const handleMenuPress = () => {
+        //action burger menu
+        console.log("menu");
+    };
+
+    return (
+        <View>
+            <View style={styles.header}>
+                <TouchableOpacity onPress={handleMenuPress}>
+                    <Ionicons style={styles.icon} name="menu" size={28} />
+                </TouchableOpacity>
+                {/* if the props title is empty show the logo */}
+                {title && !showSearch ? (
+                    <Text style={{ ...styles.title, color: colorText }}>
+                        {title}
+                    </Text>
+                ) : !showSearch ? (
+                    <Image
+                        style={{ width: 180 }}
+                        resizeMode="center"
+                        source={require("../assets/images/logo_light_mode.png")}
+                    />
+                ) : (
+                    <View style={styles.searchContainer}>
+                        <TouchableOpacity onPress={handleSearch}>
+                            <Ionicons
+                                name="search"
+                                size={28}
+                                style={styles.icon}
+                            />
+                        </TouchableOpacity>
+                        <TextInput
+                            style={styles.searchInput}
+                            placeholder="Rechercher..."
+                            onChangeText={(value) => setSearchText(value)}
+                            value={searchText}
+                            onSubmitEditing={handleSearch}
+                            autoFocus={true}
+                        />
+                        <TouchableOpacity
+                            onPress={toggleSearch}
+                            style={styles.closeButton}
+                        >
+                            <Ionicons
+                                name="close"
+                                size={24}
+                                style={styles.icon}
+                            />
+                        </TouchableOpacity>
+                    </View>
+                )}
+                {!showSearch && (
+                    <TouchableOpacity onPress={toggleSearch}>
+                        <Ionicons name="search" size={28} style={styles.icon} />
+                    </TouchableOpacity>
+                )}
+            </View>
+            <View style={styles.shadow}></View>
+        </View>
+    );
+};
+
+export default Header;
+
+const styles = StyleSheet.create({
+    searchContainer: {
+        width: "90%",
+        flexDirection: "row",
+        alignItems: "center",
+        paddingHorizontal: 15,
+        paddingVertical: 5,
+        backgroundColor: theme.colors.bg_White,
+        zIndex: 1,
+        borderWidth: 1,
+        borderColor: theme.colors.icon_gray,
+        borderRadius: 30,
+    },
+    searchInput: {
+        color: theme.colors.text_dark,
+        flex: 1,
+        height: 40,
+        paddingHorizontal: 10,
+        fontFamily: theme.fonts.openSansRegular,
+        fontSize: theme.fontSizes.medium,
+    },
+    closeButton: {
+        marginLeft: 10,
+    },
+    header: {
+        height: 80,
+        paddingHorizontal: 25,
+        backgroundColor: theme.colors.bg_White,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        zIndex: 1,
+    },
+    title: {
+        fontSize: theme.fontSizes.large,
+        fontFamily: theme.fonts.comfortaaBold,
+        width: "75%",
+        textAlign: "center",
+    },
+    icon: {
+        color: theme.colors.icon_gray,
+    },
+    shadow: {
+        backgroundColor: theme.colors.bg_White,
+        width: "100%",
+        height: 1,
+        ...Platform.select({
+            ios: {
+                shadowColor: theme.colors.text_dark,
+                shadowOffset: { width: 0, height: 12 },
+                shadowOpacity: 0.3,
+                shadowRadius: 16,
+            },
+            android: {
+                elevation: 15,
+            },
+        }),
+    },
+});
