abd67d632155ac3e8edcf1004b2a2af683aae538
pierreC <pierrecgdev@gmail.com>
2025-05-23 00:44:38 +0200

    correction header autres pages

--- screens/FollowedScreen.js
+++ screens/FollowedScreen.js
@@ -1,13 +1,18 @@
-import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import theme from "../core/theme";
 import Header from "../components/Header";
+import { useState } from "react";
 
 export default function FollowedScreen() {
+    const [searchText, setSearchText] = useState("");
     return (
         <SafeAreaView style={styles.container}>
-            <Header title={"Mes abonnements"} />
+            <Header
+                title={"Mes abonnements"}
+                inputValue={searchText}
+                setInput={setSearchText}
+            />
             <View
                 style={{
                     backgroundColor: theme.colors.bg_gray,
