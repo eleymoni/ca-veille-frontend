a01360c78b39b96b4390906afcb83e2e79e484e8
ThomasMoulins <13thomasmoulins@gmail.com>
2025-05-27 11:19:56 +0200

    Add update categories when feed is added

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -6,15 +6,18 @@ import Sections from "../components/Sections";
 import { useDispatch, useSelector } from "react-redux";
 import { useState, useEffect } from "react";
 import { getCategories } from "../constants/Urls";
+import { useIsFocused } from "@react-navigation/native";
 
 export default function CategoriesScreen() {
     const user = useSelector((state) => state.user.value);
     const [data, setData] = useState([]);
     const [searchValue, setSearchValue] = useState("");
+    const isFocused = useIsFocused();
 
     useEffect(() => {
-        getCategories(user).then((res) => setData(res.categoriesList));
-    }, []);
+        isFocused &&
+            getCategories(user).then((res) => setData(res.categoriesList));
+    }, [isFocused]);
     return (
         <View style={styles.container}>
             <Header
