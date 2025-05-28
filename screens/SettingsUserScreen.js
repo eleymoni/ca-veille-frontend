3adcf538ba46abdceaff0e84721ef561dcc75f79
ThomasMoulins <13thomasmoulins@gmail.com>
2025-05-28 11:45:03 +0200

    Add disconnect button

--- screens/SettingsUserScreen.js
+++ screens/SettingsUserScreen.js
@@ -2,12 +2,31 @@ import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import NavigationBackArrow from "../components/NavigationBackArrow";
 import theme from "../core/theme";
+import DefaultButton from "../components/DefaultButton";
+import { logout } from "../reducers/user";
+import { useDispatch } from "react-redux";
+import { useNavigation } from "@react-navigation/native";
 
 export default function SettingsUserScreen() {
+    const dispatch = useDispatch();
+    const navigation = useNavigation();
+
+    const handleDisconnect = () => {
+        dispatch(logout());
+        navigation.reset({
+            index: 0,
+            routes: [{ name: "Login" }],
+        });
+    };
+
     return (
         <SafeAreaView style={styles.container}>
             <NavigationBackArrow />
             <Text>SettingsUserScreen</Text>
+            <DefaultButton
+                text="Se déconnecter"
+                handlePress={handleDisconnect}
+            />
         </SafeAreaView>
     );
 }
