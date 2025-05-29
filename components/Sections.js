cb3af05f5c430a2035c44b9ef68b9d91146919e5
Ewa <ewa.leymoni@gmail.com>
2025-05-29 12:48:21 +0200

    fonction de recherche plus poussée

--- components/Sections.js
+++ components/Sections.js
@@ -3,17 +3,35 @@ import theme from "../core/theme";
 import ArticlesSection from "../components/ArticlesSection";
 
 export default function Sections({ data, searchText, screen }) {
+
     const renderSectionItem = ({ item }) => (
         <ArticlesSection articlesArray={item} screen={screen} />
     );
-    // filter the articles in category with the header input
-    const filteredData = data?.map((cat) => {
+
+    // je filtre sur data (catégories ou utilisateurs)
+    const filteredData = data
+    ?.filter(cat => //1ère condition !
+    (cat.name || cat.username) // nom de la cat ou nom de l'utilisateur
+    .toLowerCase()
+    .includes(searchText.toLowerCase()) || 
+    cat.articles.some( // ou 2ème condition
+        article =>
+            article.title.toLowerCase().includes(searchText.toLowerCase()) || // au moins un article a un titre ou une description qui matche
+            article.description.toLowerCase().includes(searchText.toLowerCase())
+        )
+    )
+    .map(cat => { //je tranforme chaque objet filtré pour l'affichage de la liste
+        const nameMatches = (cat.name || cat.username) // checke si le nom de la cat ou de l'utilisateur matche la recherche
+        .toLowerCase()
+        .includes(searchText.toLowerCase());
         return {
             _id: cat._id,
             name: cat.name || cat.username,
             color: cat.color || theme.colors.text_dark,
-            articles: cat.articles.filter(
-                (article) =>
+            articles: nameMatches
+            ? cat.articles // si le nom de la cat ou de l'utilisateur matche la recherche : j'affiche tous les articles de cette cat ou utilisateur
+            : cat.articles.filter( //sinon j'affiche juste les articles dont le titre ou la description matche
+                article =>
                     article.title
                         .toLowerCase()
                         .includes(searchText.toLowerCase()) ||
